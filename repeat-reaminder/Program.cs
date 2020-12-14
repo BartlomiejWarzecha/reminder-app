9f62282b6c85c581ca3a37a173397cddc513cdaf
Bartłomiej Warzecha <bartlomiejwarzecha2@gmail.com>
2020-12-14 09:35:11 +0100

    First project commit

---
+++ repeat-reaminder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace repeat_reaminder
+{
+    class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                // Open the text file using a stream reader.
+                using (var sr = new StreamReader("test.txt"))
+                {
+                    // Read the stream as a string, and write the string to the console.
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
